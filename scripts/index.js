500a42736734d5d396d5c46999ddaa6f2e293430
VICTORIA MARTINS <TORIMARTINS27@GMAIL.COM>
2024-08-19 15:46:27 -0400

    Submission for Project 4 Stage 3 08/19/2024

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,15 @@ const initialCards = [
     link: "https://plus.unsplash.com/premium_photo-1673570442940-b183c7b55ec1?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
+
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseBtn = document.querySelector(".modal__close");
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileCloseBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
